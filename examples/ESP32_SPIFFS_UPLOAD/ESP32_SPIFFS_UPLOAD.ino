22e705751144036856d2728ccece685bbc16e2ab
Ayush Sharma <asrocks5@gmail.com>
2019-01-26 04:15:17 +0530

    Simplified SPIFFS upload location

---
+++ examples/ESP32_SPIFFS_UPLOAD/ESP32_SPIFFS_UPLOAD.ino
@@ -0,0 +1,34 @@
+/*
+* ESP-DASH V2
+* Made by Ayush Sharma
+* Github URL: https://github.com/ayushsharma82/ESP-DASH
+* Support Me: https://www.patreon.com/asrocks5
+*
+* - Version Changelog - 
+* V1.0.0 - 11 Nov. 2017 - Library was Born
+* V1.0.1 - 13 Nov. 2017 - Fixed Empty SPIFFS Issue
+* V1.0.2 - 13 Nov. 2017 - Improvements on SPIFFS Issue
+* V1.0.3 - 24 Dec. 2017 - Pushing to Library Manager
+*
+* = Library Rewritten! =
+* V2.0.0 - 25 Jan 2019 - Wohoo! A breakthrough in performance and capabilities!
+*/
+
+// ================================================
+// This is a Empty Sketch. DON'T UPLOAD THE SKETCH.
+// UPLOAD SPIFFS via ESP32 SPIFFS Uploader.
+// (Skip 1 and 2 if already esp32fs-plugin installed.)
+// 1. Install https://github.com/me-no-dev/arduino-esp32fs-plugin/releases/
+// 2. Restart Arduino IDE
+// 3. Open this Sketch Again and connect your ESP32 to PC
+// 4. Click - Tools -> ESP32 Sketch Data Upload
+// 5. Finish. You need to upload spiffs for the first time. ( or when you erase SPIFFS again ) 
+
+
+void setup() {
+    
+}
+
+void loop() {
+   
+}
