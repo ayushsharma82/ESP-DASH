fea2e6928962d9ed4a5dc5a0919efb8306c24f7a
Ayush Sharma <asrocks5@gmail.com>
2019-04-10 19:08:24 +0530

    A Minimalistic About Page
    Made `About page` a less clumsy.
    And Decreased Fade Animation Time to 0.3S.
    Hopefully it will make switching between content faster :beer:

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -243,7 +243,7 @@ export default {
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .4s;
+  transition: opacity .3s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
