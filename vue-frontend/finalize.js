d3bc03982b1a2ad21b050749858822b12eab4585
Rotzbua <Rotzbua@users.noreply.github.com>
2019-04-24 00:03:40 +0200

    add no referrer (#26)
    * add no referrer
    
    Spec:
    https://w3c.github.io/webappsec-referrer-policy/#referrer-policy-delivery
    
    * add no referrer
    
    Spec:
    https://w3c.github.io/webappsec-referrer-policy/#referrer-policy-delivery

--- vue-frontend/finalize.js
+++ vue-frontend/finalize.js
@@ -23,6 +23,7 @@ let html = `
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="referrer" content="no-referrer">
     <title>ESP-DASH</title>
   </head>
   <body>
