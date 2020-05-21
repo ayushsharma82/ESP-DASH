c4ade07eeb00317b8a161aa3c5d15e7223eb6f26
Rotzbua <Rotzbua@users.noreply.github.com>
2020-05-21 18:07:14 +0200

    [Security] Forbid indexation by robots
    Dashboard can not be found by respectful search engines, if ESP is accidentally public.

--- vue-frontend/finalize.js
+++ vue-frontend/finalize.js
@@ -27,6 +27,7 @@ let html = `
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="referrer" content="no-referrer">
+    <meta name="robots" content="noindex">
     <title>ESP-DASH</title>
     ${path ? `<link rel=icon href=${path}/favicon.ico>` : ``}
     ${path ? `<link href=${path}/js/app.js rel=preload as=script>` : ``}
