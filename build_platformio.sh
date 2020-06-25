9ffb7aba45887b83ff6e816185e862002928fded
Ayush Sharma <asrocks5@gmail.com>
2020-06-25 18:10:10 +0530

    Update build_platformio.sh

--- build_platformio.sh
+++ build_platformio.sh
@@ -3,10 +3,6 @@
 set -euo pipefail
 
 pio lib -g install "ESP Async WebServer@1.2.3" "ArduinoJson@6.15.0"
-for dir in examples/ESP32_*; do
-  pio ci --lib=. --board esp32dev "${dir?}"
-done
 
-for dir in examples/ESP8266_*; do
-  pio ci --lib=. --board esp12e "${dir?}"
-done
+pio ci --lib=. --board esp32dev "examples/ESP32_basic"
+pio ci --lib=. --board esp12e "examples/ESP8266_basic"
