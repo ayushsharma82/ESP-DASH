79c23a7c06705c02afbf928438a4836acea9d19f
Ayush Sharma <asrocks5@gmail.com>
2020-10-02 23:38:49 +0530

    Updated Vue Router

--- vue-frontend/src/router.js
+++ vue-frontend/src/router.js
@@ -4,10 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 import Home from './views/Home.vue';
-import About from './views/About.vue';
 import Stats from './views/Stats.vue';
-import Contributing from './views/Contributing.vue';
-import Licensing from './views/Licensing.vue';
 // import Lock from './views/Lock.vue';
 
 export default new Router({
@@ -26,24 +23,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
       path: '/statistics',
       name: 'Statistics',
       component: Stats
-    },
-    {
-      path: '/contribute',
-      name: 'Contribute',
-      component: Contributing
-    },
-    {
-      path: '/licensing',
-      name: 'Licensing',
-      component: Licensing
     }
   ]
 })
