ea0a3c26d455a38fb6b729f206dd33241c701aef
Ayush Sharma <asrocks5@gmail.com>
2020-10-12 18:12:09 +0530

    Added Chart Example

--- build_platformio.sh
+++ build_platformio.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 pio lib -g install "ESP Async WebServer@1.2.3" "ArduinoJson@6.15.2"
 
 pio ci --lib=. --board esp32dev "examples/Basic"
+pio ci --lib=. --board esp32dev "examples/Chart"
 pio ci --lib=. --board esp32dev "examples/Dynamic"
 pio ci --lib=. --board esp32dev "examples/AccessPoint"
 pio ci --lib=. --board esp12e "examples/Basic"
+pio ci --lib=. --board esp12e "examples/Chart"
 pio ci --lib=. --board esp12e "examples/Dynamic"
 pio ci --lib=. --board esp12e "examples/AccessPoint"
