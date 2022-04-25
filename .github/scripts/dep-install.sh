087b20605404adc7cac97cd9376c101e458d8755
Ayush Sharma <asrocks5@gmail.com>
2022-04-25 19:39:35 +0530

    Create dep-install.sh

---
+++ .github/scripts/dep-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ${HOME}/Arduino/libraries
+git clone https://github.com/me-no-dev/ESPAsyncWebServer.git ESPAsyncWebServer
+git clone https://github.com/me-no-dev/ESPAsyncTCP.git ESPAsyncTCP
+git clone https://github.com/me-no-dev/AsyncTCP.git AsyncTCP
