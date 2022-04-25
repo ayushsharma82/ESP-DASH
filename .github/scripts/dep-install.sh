0421ba29becd9ad2718dd8ab83fecc27db48c33e
Ayush Sharma <asrocks5@gmail.com>
2022-04-25 19:44:33 +0530

    Update dep-install.sh

--- .github/scripts/dep-install.sh
+++ .github/scripts/dep-install.sh
@@ -3,3 +3,4 @@ cd ${HOME}/Arduino/libraries
 git clone https://github.com/me-no-dev/ESPAsyncWebServer.git ESPAsyncWebServer
 git clone https://github.com/me-no-dev/ESPAsyncTCP.git ESPAsyncTCP
 git clone https://github.com/me-no-dev/AsyncTCP.git AsyncTCP
+git clone --single-branch --branch 6.x https://github.com/bblanchon/ArduinoJson.git ArduinoJson
