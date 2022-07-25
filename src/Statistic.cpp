9850193a4d1ecc689cfa98a4629df71ee08bf6ad
Ayush Sharma <asrocks5@gmail.com>
2022-07-26 02:03:40 +0530

    - Added Custom Statistics Support - Improved ESP-DASH websocket logic - Improved memory management

---
+++ src/Statistic.cpp
@@ -0,0 +1,24 @@
+#include "Statistic.h"
+
+Statistic::Statistic(ESPDash *dashboard, const char *key, const char *value) {
+    _dashboard = dashboard;
+    #if defined(ESP8266)
+        _id = RANDOM_REG32;
+    #elif defined(ESP32)
+        _id = esp_random();
+    #endif
+    // Safe copy
+    strncpy(_key, key, sizeof(_key));
+    strncpy(_value, value, sizeof(_value));
+    _dashboard->add(this);
+}
+
+void Statistic::set(const char *key, const char *value) {
+    // Safe copy
+    strncpy(_key, key, sizeof(_key));
+    strncpy(_value, value, sizeof(_value));
+}
+
+Statistic::~Statistic() {
+    _dashboard->remove(this);
+}
