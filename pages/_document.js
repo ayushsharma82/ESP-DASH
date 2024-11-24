8e40e3689f01cbdca6a7af97126a06a0bbac3150
Ayush Sharma <asrocks5@gmail.com>
2024-11-24 18:25:54 +0530

    feat: add promo banner

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,49 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+const promo_banner = [
+  "Black Friday 2024 Sale is Live!",
+  "Flat 33% off on Advantage Bundle",
+  "Upgrade Now at Lowest Price Ever!"
+];
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
+      {
+          promo_banner && (
+            <div className="relative flex overflow-x-hidden text-zinc-200 font-semibold text-sm uppercase bg-indigo-600">
+              <div className="py-2 animate-marquee whitespace-nowrap">
+                {
+                  [1,2,3].map((_, i) => (
+                    <span key={'pb-'+i} className='mx-6'>
+                      {
+                        promo_banner.map((item, i) => (
+                          <span className="mx-4" key={'pbs-'+i}>{item}</span>
+                        ))
+                      }
+                    </span>
+                  ))
+                }
+              </div>
+
+              <div className="absolute top-0 py-2 animate-marquee2 whitespace-nowrap">
+                {
+                  [1, 2, 3].map((_, i) => (
+                    <span key={'pba-' + i} className='mx-6'>
+                      {
+                        promo_banner.map((item, i) => (
+                          <span className="mx-4" key={'pbas-' + i}>{item}</span>
+                        ))
+                      }
+                    </span>
+                  ))
+                }
+              </div>
+            </div>
+          )
+        }
         <Main />
         <NextScript />
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,21 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [],
